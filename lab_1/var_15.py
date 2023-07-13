441666ccf1a74c1e63a07e49949827c39bae5c1c
andreyyy363 <shtepura2605@outlook.com>
2023-07-13 23:26:22 +0300

    adding lab 1

---
+++ lab_1/var_15.py
@@ -0,0 +1,60 @@
+print('Lab_1')
+print('Var_15')
+print('-----------------------------------')
+
+
+# Generating array
+def generating_random_array(row, col):
+    # Random generating
+    from random import randint
+    arr = [[randint(-10, 10) for _ in range(col)] for _ in range(row)]
+    return arr
+
+
+# Printing array
+def print_array(arr):
+    print('Random array:')
+    print('-----------------------------------')
+    for i in arr:
+        for j in i:
+            print(f'{j:4}', end=' ')
+        print()
+    print('-----------------------------------')
+
+
+# Exercise 1
+def find_zero(arr, row, col):
+    found = False
+    for i in range(row):
+        for j in range(col):
+            if arr[i][j] == 0:
+                print('First null element found in ', i + 1, ' line.')
+                found = True
+                break
+        if found:
+            break
+    if not found:
+        print('Null elements not found.')
+    print('-----------------------------------')
+
+
+# Exercise 2
+def sum_of_negative_paired_elements(arr, row, col):
+    sum_of_elements = [0] * row
+    for i in range(row):
+        for j in range(col):
+            if arr[i][j] < 0 and arr[i][j] % 2 == 0:
+                sum_of_elements[i] += arr[i][j]
+
+    sum_of_elements.sort()
+    print('Sum of even negative elements\narranged in descending order:')
+    print(sum_of_elements)
+    print('-----------------------------------')
+
+
+n = input('Enter the number of rows: ')
+m = input('Enter the number of columns: ')
+array = generating_random_array(int(n), int(m))
+print_array(array)
+find_zero(array, int(n), int(m))
+sum_of_negative_paired_elements(array, int(n), int(m))
